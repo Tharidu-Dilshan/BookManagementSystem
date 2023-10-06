b432605636d6ebd393fc3b6d510725e75ecfb082
Tharidu D Wimalasena <t.dilsh.99@gmail.com>
2023-10-06 23:58:51 +0530

    Front End Done

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 
-// Load environment variables from .env file
 dotenv.config();
 
 const PORT = process.env.PORT || 8070;
 
-// Middleware setup
 app.use(cors());
 app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
-// Connect to MongoDB using Mongoose
 mongoose.connect(URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
@@ -24,12 +21,10 @@ mongoose.connect(URL, {
 
 const connection = mongoose.connection;
 
-// Event listener for successful MongoDB connection
 connection.once("open", () => {
   console.log("MongoDB connection successful!");
 });
 
-// Route for handling books
 const bookRouter = require("./routes/book.js");
 app.use("/book", bookRouter);
 
